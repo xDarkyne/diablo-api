35734789fa49da0c5cfcc3b18000cce913df1684
xDarkyne <darkyne.gaming@outlook.de>
2021-10-19 15:02:54 +0200

    feat(api): setup sample api

--- controllers/diablo.controller.ts
+++ controllers/diablo.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Cache, CacheContainer } from "node-ts-cache";
 import { MemoryStorage } from 'node-ts-cache-storage-memory';
 import Config from '../config/config';
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { stringify } from "qs";
 
 const tokenCache = new CacheContainer(new MemoryStorage());
@@ -10,10 +10,20 @@ const tokenCache = new CacheContainer(new MemoryStorage());
 export class DiabloController{
 
     public static Endpoints: Record<string, string> = {
-        "seasonIndex": "https://eu.api.blizzard.com/data/d3/season/"
+        "seasonIndex": "https://eu.api.blizzard.com/data/d3/season/",
+        "itemTypeIndex": "https://eu.api.blizzard.com/d3/data/item-type/",
+        "item": "https://eu.api.blizzard.com/d3/data/item/"
+    }
+    public static HeroSlugs: Record<string, string> = {
+        "barbarian": "barbarian",
+        "crusader": "crusader",
+        "necromancer": "necromancer",
+        "dh": "demon-hunter",
+        "wd": "witch-doctor",
+        "monk": "monk"
     }
 
-    @Cache(tokenCache, {ttl: 10})
+    @Cache(tokenCache, { ttl: 15 })
     protected static async getAccessToken(): Promise<any> {
         if (Config.CLIENT_ID === "" || Config.CLIENT_SECRET === "") throw "clldd";
 
@@ -28,22 +38,48 @@ export class DiabloController{
             data: stringify({"grant_type": "client_credentials"})
         };
 
-        let data = await axios.request(requestData).then((res: AxiosResponse) => res.data).catch(error => console.log(error));
+        let data = await axios.request(requestData).then((res: AxiosResponse) => {
+            return res.data;
+        }).catch(error => console.log(error));
         const token = data.access_token;
+        const headers: AxiosRequestConfig = {
+            headers: { "Authorization": `bearer ${token}` }
+        }
 
-        return token;
+        return headers;
     }
 
-    public static getSeasonIndex = async(req: Request, res: Response) => {
-        let token = await this.getAccessToken();
-        let auth = {
-            "Authorization": `Bearer ${token}`
-        };
+    public static async getItemType(req: Request, res: Response) {
+        let slug = req.params.type;
+        let auth = await this.getAccessToken();
+        let url = this.Endpoints["itemTypeIndex"]+slug;
 
-        let data = await axios.get(this.Endpoints["seasonIndex"], { headers: auth }).then(res => res.data);
+        let data = await axios.get(url, auth).then(res => res.data).catch((err: AxiosError) => {
+            console.error(err);
+        });
 
-        data = await axios.get("https://eu.api.blizzard.com/data/d3/season/24", { headers: auth }).then(res => res.data);
-        data = await axios.get(data._links.self.href, { headers: auth }).then(res => res.data);
+        res.json(data);
+    }
+
+    public static async getItem(req: Request, res: Response) {
+        let slug = req.params.item;
+        let auth = await this.getAccessToken();
+        let url = this.Endpoints["item"]+slug;
+
+        let data = await axios.get(url, auth).then(res => res.data).catch((err: AxiosError) => {
+            console.error(err);
+        });
+
+        res.json(data);
+    }
+
+    public static getItemTypeIndex = async(req: Request, res: Response) => {
+        let auth = await this.getAccessToken();
+
+        let data = await axios.get("https://eu.api.blizzard.com/d3/data/item-type/", auth).then(res => res.data).catch((err: AxiosError) => {
+
+        });
+
         res.json(data);
     }
 }
--- routes/diablo.routes.ts
+++ routes/diablo.routes.ts
@@ -7,6 +7,8 @@ DiabloRouter.get('/hello', (req: Request, res: Response,) => {
     res.send("<p>Hello World</p>");
 });
 
-DiabloRouter.get('/token', DiabloController.getSeasonIndex)
+DiabloRouter.get('/item-types', (req, res) => DiabloController.getItemTypeIndex(req, res))
+DiabloRouter.get('/item-types/:type', (req, res) => DiabloController.getItemType(req, res))
+DiabloRouter.get('/items/:item', (req, res) => DiabloController.getItem(req, res))
 
 export = DiabloRouter;
